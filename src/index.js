3899efcbe4bebb0a2d43101322980247f17272ca
krishna9788 <krishnakushwaha978@gmail.com>
2023-03-31 23:11:33 +0530

    assign

--- src/index.js
+++ src/index.js
@@ -4,6 +4,31 @@ import App from "./components/App";
 //code here 
 
 
+// import React from 'react';
+
+// import ReactDOM from 'react-dom/client';
+
+// import App from './App';
+
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+
+<Provider store={store}>
+
+<App />
+
+</Provider>
+
+);
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+
+// ReactDOM.render(<App />, document.getElementById("root"));
